9f04c35de97549abf9c0e15fcfa07ab3967a0a59
AbdelmajidEMI <hou.abd.2015@gmail.com>
2023-11-28 15:13:31 +0100

    Completing the form of shoosing ingredients but still submit don't work

--- src/main/java/sia/tacocloud/modules/Taco.java
+++ src/main/java/sia/tacocloud/modules/Taco.java
@@ -1,12 +1,14 @@
 package sia.tacocloud.modules;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
-    private String name; // Name of the taco
-    private List<Ingredient> ingredients; // List of ingredients in the taco
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
+    private String name;
+    private List<String> ingredients;
 }
