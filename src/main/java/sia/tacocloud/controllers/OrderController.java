4bfd05aec3b7eb9924f00532afe334fdcf1b7825
AbdelmajidEMI <hou.abd.2015@gmail.com>
2023-12-01 11:15:51 +0100

    Adding some restrictions on the taco class's parameters

--- src/main/java/sia/tacocloud/controllers/OrderController.java
+++ src/main/java/sia/tacocloud/controllers/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import org.springframework.validation.Errors;
 import sia.tacocloud.modules.Order;
 
